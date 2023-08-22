7238a5fb64de8d87329e56f88c1efa298dfff47b
Gintautas Poderys <gintautasp25@gmail.com>
2023-08-22 22:16:34 +0300

    completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let k = 1; k < arguments.length; k++){
+        // loop through all possible arguments
+        arg = arguments[k]
+        // use current argument to check if it needs to be spliced from array
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arg ) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
