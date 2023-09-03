b933a9cfae1c8e645ed1b731def6d5a76eacee68
Gintautas Poderys <gintautasp25@gmail.com>
2023-09-03 14:48:09 +0300

    completed palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let str2 = []
+    str2 = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    str2 = str2.split('')
+    str2 = str2.map(item => item.toLowerCase())
+     for (let i = 1; i < str2.length+1; i++){
+        if (str2[i-1] !== str2[str2.length-i]){
+            return false
+        }
+    }
+    return  true
 };
 
 // Do not edit below this line
