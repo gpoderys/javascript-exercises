d72a1a51e326e24e51b6da1abe64e3f310c4cb16
Gintautas Poderys <gintautasp25@gmail.com>
2023-09-03 15:54:33 +0300

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return 'OOPS'
+    let current = 1
+    let previous = 0
+    for (let i = 1; i < index; i++){
+        let result = current  + previous
+        previous = current
+        current = result
+    }
+    return current
 };
 
 // Do not edit below this line
